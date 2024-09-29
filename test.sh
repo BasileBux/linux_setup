9ad780d3dbd17f6e1820233a959b858e27c4e3aa
BasileBux <basile.buxtorf@ik.me>
2024-09-29 14:54:30 +0200

    testing

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ if [ "$userAnswer" = "y" ] || [ "$userAnswer" = "Y" ]; then
     ownedRepo=true
 elif [ "$userAnswer" = "n" ] || [ "$userAnswer" = "N" ]; then
     ownedRepo=false
-else
-    echo "Aborting installation, your choice wasn't right."
-    exit 1
+# else
+#     echo "Aborting installation, your choice wasn't right."
+#     exit 1
 fi
