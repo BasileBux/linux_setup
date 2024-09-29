9326988f401ef66ab57834808b7a0cdd08f22dc6
BasileBux <basile.buxtorf@ik.me>
2024-09-29 14:53:12 +0200

    testing

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Do you have write access to the dotfiles repo ? (Y/N) > " userAnswer
+if [ "$userAnswer" = "y" ] || [ "$userAnswer" = "Y" ]; then
+    ownedRepo=true
+elif [ "$userAnswer" = "n" ] || [ "$userAnswer" = "N" ]; then
+    ownedRepo=false
+else
+    echo "Aborting installation, your choice wasn't right."
+    exit 1
+fi
